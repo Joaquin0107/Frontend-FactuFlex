a7780e1705c122cf926372a67ca9c5824e5fb68a
Joaquín Emilio Huayllasco López <joaquin20040107@gmail.com>
2024-11-20 16:31:16 -0500

    Implementación login y landing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { NgIf } from '@angular/common';
+import { LoginService } from './services/login.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    RouterLink,
+    NgIf,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Frontend-FactuFlex';
+  role: string = '';
+  constructor(private loginService: LoginService) {}
+
+  cerrar() {
+    sessionStorage.clear();
+  }
+
+  verificar() {
+    this.role = this.loginService.showRole();
+    return this.loginService.verificar();
+  }
+  isGerente() {
+    return this.role === 'GERENTE';
+  }
+
+  isOperario() {
+    return this.role === 'OPERARIO';
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { segGuard } from './guard/seguridad.guard';
+import { LandingComponent } from './components/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'landing',
+    component: LandingComponent,
+    canActivate: [segGuard],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
