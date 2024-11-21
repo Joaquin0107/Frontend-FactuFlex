c96fe086e051d5448206f569cdcb3384affc94dd
Joaquín Emilio Huayllasco López <joaquin20040107@gmail.com>
2024-11-21 01:22:04 -0500

    html completos - falta css

--- src/app/components/cliente/crear-cliente/crear-cliente.component.ts
+++ src/app/components/cliente/crear-cliente/crear-cliente.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-crear-cliente',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatFormField,
+    MatLabel,
+    MatDividerModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './crear-cliente.component.html',
-  styleUrl: './crear-cliente.component.css'
+  styleUrl: './crear-cliente.component.css',
 })
-export class CrearClienteComponent {
-
-}
+export class CrearClienteComponent {}
--- src/app/components/operacion/crear-operacion/crear-operacion.component.ts
+++ src/app/components/operacion/crear-operacion/crear-operacion.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-crear-operacion',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatFormField,
+    MatLabel,
+    MatDividerModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './crear-operacion.component.html',
   styleUrl: './crear-operacion.component.css'
 })
