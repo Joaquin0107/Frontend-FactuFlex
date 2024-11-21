76db38ca78ac5057cc28209d6da51e0251b4a128
Joaquín Emilio Huayllasco López <joaquin20040107@gmail.com>
2024-11-21 12:18:15 -0500

    cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { LoginComponent } from './components/login/login.component';
 import { segGuard } from './guard/seguridad.guard';
 import { LandingComponent } from './components/landing/landing.component';
 import { CrearClienteComponent } from './components/cliente/crear-cliente/crear-cliente.component';
-import { ListarReporteComponent } from './components/reporte/listar-reporte/listar-reporte.component';
 import { ListarOperacionComponent } from './components/operacion/listar-operacion/listar-operacion.component';
 import { CrearOperacionComponent } from './components/operacion/crear-operacion/crear-operacion.component';
 import { ListarClienteComponent } from './components/cliente/listar-cliente/listar-cliente.component';
@@ -40,8 +39,4 @@ export const routes: Routes = [
     path:'ver-operacion',
     component:ListarOperacionComponent,
   },
-  {
-    path:'ver-reportes',
-    component:ListarReporteComponent,
-  }
 ];
